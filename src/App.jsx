8c966e6c7b2f3c712352cd295ecbc58c51807e38
sp41414 <134581084+sp41414@users.noreply.github.com>
2025-12-13 02:29:22 +0400

    feat: add more projects and technologies

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import ThemeToggle from "./components/ThemeToggle"
 import Flag from "./components/Flag"
 import AvailableDot from "./components/AvailableDot"
 import TechStack from "./components/TechStack"
-import { ExternalLink } from "lucide-react"
 import ProjectCards from "./components/ProjectCards"
 import Contact from "./components/Contact"
 import SectionScroller from "./components/SectionScroller"
@@ -12,7 +11,10 @@ function App() {
     return (
         <ThemeProvider>
             <div className="flex flex-col min-h-screen bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-100 transition-colors duration-200">
-                <SectionScroller />
+                {/*
+                    add back when the portfolio is a little bit... bigger..
+                    <SectionScroller />
+                */}
                 <nav className="sticky top-0 z-1 bg-white/80 dark:bg-neutral-900/80 backdrop-blur-sm border-b border-neutral-200 dark:border-neutral-800">
                     <div className="max-w-5xl mx-auto px-6 py-4 flex justify-end">
                         <ThemeToggle />
@@ -23,7 +25,7 @@ function App() {
                     <div className="space-y-8">
                         <section id="intro" className="space-y-4">
                             <h1 className="font-semibold text-4xl sm:text-5xl lg:text-6xl tracking-tight leading-tight">
-                                Hey, I'm <span className="text-yellow-400 underline">[NAME]</span>!
+                                Hey, I'm <span className="text-yellow-400 underline">Husain</span>!
                             </h1>
                             <p className="text-lg sm:text-xl text-neutral-600 dark:text-neutral-400 flex items-center gap-2 flex-wrap">
                                 Fullstack Developer <span className="text-neutral-400 dark:text-neutral-600">•</span> United Arab Emirates <Flag />
